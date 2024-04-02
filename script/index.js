cee124a13117079ebafacb7569c6d7f9f72ce268
Zishan Ansari <zishana@axioned.com>
2024-04-03 03:31:32 +0530

    ref: Removed widget-area

--- script/index.js
+++ script/index.js
@@ -10,7 +10,6 @@ customElements.define("ai-widget", AIWidget);
 
 document.addEventListener("DOMContentLoaded", function () {
   const widgetContainer = document.getElementById("ai-widget-container");
-  const widgetArea = document.createElement("div");
   const FAB = document.createElement("button");
   const arrowIcon = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-chevron-up"><path d="m18 15-6-6-6 6"/></svg>`;
   const crossIcon = `<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-x"><path d="M18 6 6 18"/><path d="m6 6 12 12"/></svg>`;
@@ -31,9 +30,6 @@ document.addEventListener("DOMContentLoaded", function () {
     overflow: hidden;
   `;
 
-  widgetArea.classList.add("widget-area");
-  widgetArea.style.cssText = `width: 100%; height: 100%;`;
-
   FAB.id = "FAB";
   FAB.innerHTML = arrowIcon;
   FAB.style.cssText = `
@@ -53,13 +49,12 @@ document.addEventListener("DOMContentLoaded", function () {
     transition: all 0.3s ease-in-out;
   `;
 
-  widgetContainer.appendChild(widgetArea);
   document.body.appendChild(FAB);
 
   let aIWidget = document.createElement("ai-widget");
   aIWidget.id = "aIWidget";
   aIWidget.style.cssText = `display: block; width: 100%; height: 100%;`;
-  widgetArea.appendChild(aIWidget);
+  widgetContainer.appendChild(aIWidget);
 
   const toggleWidget = () => {
     const isOpen = aIWidget.classList.toggle("open");
